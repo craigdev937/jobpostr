1a313b567f5e4d02eaed46a745ca752f6e76418a
indigomx9 <indigomx9@gmail.com>
2018-09-28 00:49:06 -0400

    Added a user controller, updated the routes and added a new template.

--- app/Validators/CreateUser.js
+++ app/Validators/CreateUser.js
@@ -8,17 +8,16 @@ class CreateUser {
             'password': 'required'
         }
     }
-    
+
     get messages () {
         return {
-            'required': 'Woah now, {{ field }} is required.'
+            'required': 'Woah now, {{ field }} is required.',
             'unique': 'Wait a second, the {{ field }} already exists.'
         }
     }
 
     async fails(error) {
-        this.ctx.session.withErrors(error)
-            .flashAll();
+        this.ctx.session.withErrors(error).flashAll();
         return this.ctx.response.redirect('back');
     }
 };
