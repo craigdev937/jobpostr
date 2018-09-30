3cd31454b5e9180d3e79191cf504e1bafde8e0ff
indigomx9 <indigomx9@gmail.com>
2018-09-29 22:53:27 -0400

    Updated the templates, controllers and added a new validator.

--- app/Validators/LoginUser.js
+++ app/Validators/LoginUser.js
@@ -1,26 +1,22 @@
 'use strict'
 
-class CreateUser {
+class LoginUser {
     get rules () {
         return {
-            'username': 'required|unique:users',
-            'email': 'required|unique:users',
-            'password': 'required|min:6'
+            'email': 'required|email',
+            'password': 'required'
         }
     }
-
     get messages () {
         return {
             'required': 'Woah now, {{ field }} is required.',
-            'unique': 'Wait a second, the {{ field }} already exists.'
         }
     }
-
     async fails(error) {
         this.ctx.session.withErrors(error).flashAll();
         return this.ctx.response.redirect('back');
     }
 };
 
-module.exports = CreateUser;
+module.exports = LoginUser;
 
